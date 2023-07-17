715df283be07e969ce238064d8ef3bae7b89f1a2
newbee1939 <hideto3970@gmail.com>
2023-07-18 08:30:18 +0900

    更新

---
+++ contracts/ERC20.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+// ERC20トークンのサンプル
+contract ERC20 {
+    // 全てのトークンの数
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    address public _owner; //temporary public
+
+    constructor (string memory name_, string memory symbol_, uint8 decimals_) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+        _owner = msg.sender;
+    }
+
+    // externalすると外部からも呼び出せる
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() external view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSypply() external view returns (uint256) {
+        return _totalSupply;
+    }
+}
